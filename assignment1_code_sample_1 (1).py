cf9844675acd10835fc74dd4a0528879f96d8721
rrc_f2024_schoudhary <schoudhary4@rrc.ca>
2026-02-10 10:34:47 -0600

    Update assignment1_code_sample_1 (1).py

--- assignment1_code_sample_1 (1).py
+++ assignment1_code_sample_1 (1).py
@@ -5,23 +5,23 @@ from urllib.request import urlopen
 db_config = {
     'host': 'mydatabase.com',
     'user': 'admin',
-    'password': 'secret123'
+    'password': 'secret123' - ##OWASP A02 – Cryptographic Failures.
 }
 
 def get_user_input():
-    user_input = input('Enter your name: ')
+    user_input = input('Enter your name: ') ##OWASP A04 – Insecure Design
     return user_input
 
 def send_email(to, subject, body):
-    os.system(f'echo {body} | mail -s "{subject}" {to}')
+    os.system(f'echo {body} | mail -s "{subject}" {to}') ##OWASP A03 – Injection
 
 def get_data():
-    url = 'http://insecure-api.com/get-data'
+    url = 'http://insecure-api.com/get-data' ##OWASP A02 – Cryptographic Failures
     data = urlopen(url).read().decode()
     return data
 
 def save_to_db(data):
-    query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
+    query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')" ##OWASP A03 – Injection
     connection = pymysql.connect(**db_config)
     cursor = connection.cursor()
     cursor.execute(query)
